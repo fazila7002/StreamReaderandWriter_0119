851b1901ed83e04200f7a075f9f0f00e73e5f731
fazila7002 <fazilatunnisa705@gmail.com>
2026-06-02 17:51:12 +0700

    loop input dan penulisan ke file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -12,6 +12,12 @@ int main()
 
     cout << ">=Menulis file, \'q\' untuk keluar" << endl;
 
-
+    while (true)
+    {
+        cout << "- ";
+        getline(cin,baris);
+        if (baris == "q") break;
+        outfile << baris << endl;
+    }
 
 }
