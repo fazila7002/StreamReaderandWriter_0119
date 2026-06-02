2b3058754cf0f057aee9cc31ccae919d4644cbb5
fazila7002 <fazilatunnisa705@gmail.com>
2026-06-02 18:31:59 +0700

    menambahkan penanganan exception dengan catch spesifik dan default

--- ManualException.cpp
+++ ManualException.cpp
@@ -8,4 +8,17 @@ int main()
         cout << "Selamat belajar di prodi TI UMY" << endl;
         cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
+
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+        
+    }
+
+    catch (...)
+    {
+        cout << "default pengecualian dieksekusi" << endl;
+
+    }
+    return 0;
 }
